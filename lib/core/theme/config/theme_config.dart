acd63835a31382abe073e3b36a8f7f04f849a222
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2024-12-31 15:03:00 +0200

    refactor: refactor app global theme

---
+++ lib/core/theme/config/theme_config.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ThemeConfig {
+  static ThemeData get lightTheme {
+    return ThemeData.from(
+      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4E833B)),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData.from(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xFF4E833B),
+        brightness: Brightness.dark,
+      ),
+    );
+  }
+
+  static SystemUiOverlayStyle systemUiOverlayStyle(bool isDarkMode) {
+    return SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: isDarkMode ? Brightness.light : Brightness.dark,
+      statusBarBrightness: isDarkMode ? Brightness.dark : Brightness.light,
+      systemNavigationBarColor: isDarkMode
+          ? darkTheme.scaffoldBackgroundColor
+          : lightTheme.scaffoldBackgroundColor,
+      systemNavigationBarIconBrightness:
+          isDarkMode ? Brightness.light : Brightness.dark,
+      systemNavigationBarDividerColor: Colors.transparent,
+    );
+  }
+}
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pulsehub/core/routing/router.dart';
+import 'package:pulsehub/core/theme/config/theme_config.dart';
 import 'package:pulsehub/core/theme/cubit/theme_cubit.dart';
 
 class MyApp extends StatelessWidget {
@@ -13,40 +14,16 @@ class MyApp extends StatelessWidget {
       create: (context) => ThemeCubit(),
       child: BlocBuilder<ThemeCubit, bool>(
         builder: (context, isDarkMode) {
-          final theme = isDarkMode ? ThemeData.from(
-              colorScheme: ColorScheme.fromSeed(
-                seedColor: const Color(0xFF4E833B),
-                brightness: Brightness.dark,
-              ),
-            ) : ThemeData.from(
-              colorScheme:
-                  ColorScheme.fromSeed(seedColor: const Color(0xFF4E833B)),
-            );
-
+          // Set the system UI overlay style based on the theme
           SystemChrome.setSystemUIOverlayStyle(
-            SystemUiOverlayStyle(
-              statusBarColor: Colors.transparent,
-              statusBarIconBrightness: isDarkMode ? Brightness.light : Brightness.dark,
-              statusBarBrightness: isDarkMode ? Brightness.dark : Brightness.light,
-              systemNavigationBarColor: theme.scaffoldBackgroundColor,
-              systemNavigationBarIconBrightness: isDarkMode ? Brightness.light : Brightness.dark,
-              systemNavigationBarDividerColor: Colors.transparent,
-            ),
+            ThemeConfig.systemUiOverlayStyle(isDarkMode),
           );
 
           return MaterialApp.router(
             routerConfig: router,
             debugShowCheckedModeBanner: false,
-            theme: ThemeData.from(
-              colorScheme:
-                  ColorScheme.fromSeed(seedColor: const Color(0xFF4E833B)),
-            ),
-            darkTheme: ThemeData.from(
-              colorScheme: ColorScheme.fromSeed(
-                seedColor: const Color(0xFF4E833B),
-                brightness: Brightness.dark,
-              ),
-            ),
+            theme: ThemeConfig.lightTheme,
+            darkTheme: ThemeConfig.darkTheme,
             themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
           );
         },
