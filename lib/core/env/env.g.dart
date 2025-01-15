a09466dadc392cb6db5005e45e27cc982fc51445
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2025-01-15 19:30:18 +0200

    fix(env): update development base URL for consistency with production

--- lib/core/env/env.g.dart
+++ lib/core/env/env.g.dart
@@ -10,7 +10,7 @@ part of 'env.dart';
 // ignore_for_file: type=lint
 // generated_from: .env
 final class _Env {
-  static const String devBaseUrl = 'https://dev-pulsehub.synology.me:9099';
+  static const String devBaseUrl = 'https://pulsehub.synology.me:9099';
 
   static const String prodBaseUrl = 'https://pulsehub.synology.me:9099';
 }
